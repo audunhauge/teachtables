f9276376c684e013980efc8a2bcf95a72b00c7a0
Audun Hauge <audun.hauge@gmail.com>
2013-03-14 10:52:05 +0100

    details for plain.js

--- public/js/mylibs/plain.js
+++ public/js/mylibs/plain.js
@@ -3,8 +3,8 @@
 // must be small and agile
 
 var $j = jQuery.noConflict();
-var yearplan;           
-var heldag;           
+var yearplan;
+var heldag;
 var dager = "Man Tir Ons Tor Fre Merknad".split(" ");
 var offset = Url.decode(gup("offset"));
 var user = Url.decode(gup("navn"));
@@ -15,14 +15,14 @@ var uname       = $j("#uname").html();
 var firstname   = $j("#firstname").html().caps();
 var lastname    = $j("#lastname").html().caps();
 
-var category = { "1BEV2":11, "1BID5":4, "1DAT5":11, "1ENG5":2, "1FR24":2, "1GEO2":1, "1KRO2":4, "1LYT2":10, "1MDD5":11, 
-    "1MP5":1, "1MT5":1, "1MUS5":10, "1NAT5":1, "1NOR4":2, "1SFF3":3, "1SP24":2, "1TEN5":12, "1TY14":2, "1TY24":2, 
-    "2BID5":4, "2BIO5":1, "2DAP5":11, "2EB12":11, "2FR24":2, "2FYS5":1, "2GEO2":1, "2GEO5":1, "2GTD2":11, "2HFI5":3, "2HIS2":3, 
-    "2IEN5":2, "2INF5":1, "2KJE5":1, "2KRO2":4, "2MAP3":1, "2MAR5":1, "2MAS5":1, "2MFD5":10, "2MIK5":3, "2MP15":10, "2NOR4":3, 
-    "2OKS5":3, "2RTL5":3, "2SCD5":11, "2SDF5":11, "2SFF3":3, "2SGE5":3, "2SP24":2, "2SSA5":3, "2TEB2":11, "2TEF5":12, "2TEP5":12, 
-    "2TFO5":1, "2TIP5":12, "2TY14":2, "2TY24":2, "3BIO5":1, "3DAP5":11, "3EB12":11, "3ELK5":2, "3FYS5":1, "3GEOF5":1, 
-    "3GTD2":11, "3HFI5":3, "3HIS4":3, "3IKS5":10, "3INF5":1, "3IOL5":12, "3KJEM5":1, "3KRO2":4, "3MAR5":1, "3MAS5":1, "3MFD5":10, 
-    "3MIK5":3, "3MP15":10, "3NOR6":2, "3OKL5":3, "3PMR5":3, "3REL3":3, "3RTL5":3, "3SAE5":3, "3SCD5":11, "3SDA5":11, "3SDF5":11, 
+var category = { "1BEV2":11, "1BID5":4, "1DAT5":11, "1ENG5":2, "1FR24":2, "1GEO2":1, "1KRO2":4, "1LYT2":10, "1MDD5":11,
+    "1MP5":1, "1MT5":1, "1MUS5":10, "1NAT5":1, "1NOR4":2, "1SFF3":3, "1SP24":2, "1TEN5":12, "1TY14":2, "1TY24":2,
+    "2BID5":4, "2BIO5":1, "2DAP5":11, "2EB12":11, "2FR24":2, "2FYS5":1, "2GEO2":1, "2GEO5":1, "2GTD2":11, "2HFI5":3, "2HIS2":3,
+    "2IEN5":2, "2INF5":1, "2KJE5":1, "2KRO2":4, "2MAP3":1, "2MAR5":1, "2MAS5":1, "2MFD5":10, "2MIK5":3, "2MP15":10, "2NOR4":3,
+    "2OKS5":3, "2RTL5":3, "2SCD5":11, "2SDF5":11, "2SFF3":3, "2SGE5":3, "2SP24":2, "2SSA5":3, "2TEB2":11, "2TEF5":12, "2TEP5":12,
+    "2TFO5":1, "2TIP5":12, "2TY14":2, "2TY24":2, "3BIO5":1, "3DAP5":11, "3EB12":11, "3ELK5":2, "3FYS5":1, "3GEOF5":1,
+    "3GTD2":11, "3HFI5":3, "3HIS4":3, "3IKS5":10, "3INF5":1, "3IOL5":12, "3KJEM5":1, "3KRO2":4, "3MAR5":1, "3MAS5":1, "3MFD5":10,
+    "3MIK5":3, "3MP15":10, "3NOR6":2, "3OKL5":3, "3PMR5":3, "3REL3":3, "3RTL5":3, "3SAE5":3, "3SCD5":11, "3SDA5":11, "3SDF5":11,
     "3TEB2":11, "3TEF5":12, "3TEP5":12, "3TFO5":1, "3TIP5":12, "ENSE2":10, "KOMO":20 };
 
 
@@ -128,7 +128,7 @@ function drawStarbCourse(data,thisweek) {
       }
     }
     s += '<tr id="starbrow"><th colspan="6" title="click to hide/show">'
-            + 'Starbkurs <span class="info">(peker over for detaljer)</span></th></tr>';
+            + 'Starbkurs denne uka <span class="info">(peker over for detaljer)</span></th></tr>';
     s += '<tr>';
     for (var i=0;i<6;i++) {
         if (header[i]) {
@@ -184,7 +184,7 @@ function drawAbsentees(data,thisweek) {
                 title += ' ' + abbs.name + ' ' + abbs.value + ' time';
               }
               header[j] += '<span class="'+absclass+'" title="'+title+'" >'+teach.username+'</span>';
-            } 
+            }
         }
         // add absentees for klasses
         for (var kl in klasstab[j]) {
@@ -204,7 +204,7 @@ function drawAbsentees(data,thisweek) {
       }
     }
     wholeweek += '</ul>';
-    s += "<tr><th colspan=5>Fravær: <span class=\"info\">"
+    s += "<tr><th colspan=5>Fravær denne uka: <span class=\"info\">"
       + " (<span class=\"red\">rød</span>:hele dagen, <span class=\"green\">grønn</span>:timer, <span class=\"klassabs\">aqua:</span>Klasser</span>)</th>"
       + "<th>Hele uka</th></tr>";
     s += "<tr>";
@@ -242,7 +242,9 @@ function getYearPlanThisWeek(thisweek) {
         s += "<th>" + dager[i] + "</th>";
     }
     s += "</tr>";
+    var title = 'Denne Uka';
     for (var w=0;w<2;w++) {
+      s += '<tr><th colspan=6><span style="font-size:0.7em;" >'+title+'</span></th></tr>';
       s += "<tr>";
       var greg = julian.jdtogregorian(thisweek+w*7);
       var un = '<span title="'+greg.day+'.'+greg.month+'.'+greg.year+'" class="ukenr">U' +julian.week(thisweek+w*7)+ '</span>';
@@ -251,6 +253,7 @@ function getYearPlanThisWeek(thisweek) {
           s += "<td class=\"dayinfo\">" + header[w][i] + "</td>";
       }
       s += "</tr>";
+      title = 'Neste Uke';
     }
     return s;
 }
