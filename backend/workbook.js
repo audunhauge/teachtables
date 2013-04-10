f4ea7e6764847957ffff4c60569814aa86df25cc
Audun Hauge <audun.hauge@gmail.com>
2013-04-10 15:10:44 +0200

    get score for progress

--- backend/workbook.js
+++ backend/workbook.js
@@ -762,7 +762,7 @@ var progressview = exports.progressview = function(user,query,callback) {
   console.log("select qu.userid, qu.cid,count(qu.score) from quiz_useranswer qu inner join quiz_question q on (q.id = qu.qid) "
       + "where q.teachid=$2 and q.subject=$1 and qu.attemptnum > 0 and qu.userid in ("+studlist+") group by qu.userid,qu.cid order by userid,cid", [subject,user.id]);
   if (isteach) {
-      client.query("select q.name as n,qu.userid as u, qu.cid as k,count(qu.score) as c "
+      client.query("select q.name as n,qu.userid as u, qu.cid as k,count(qu.score) as c,sum(qu.score) as s "
       + "from quiz_useranswer qu inner join quiz_question q on (q.id = qu.cid) "
       + "where q.qtype = 'quiz' and q.teachid=$2 and q.subject=$1 and qu.attemptnum > 0 and qu.userid in ("+studlist
       + ") group by qu.userid,qu.cid,q.name order by userid,cid", [subject,user.id],
