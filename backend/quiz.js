bfe625a9d09cbce74091e8122f1b52890567ace5
Audun Hauge <audun.hauge@gmail.com>
2013-05-27 08:30:42 +0200

    fix to sympify: -- is +, 1*x is x

--- backend/quiz.js
+++ backend/quiz.js
@@ -67,6 +67,13 @@ function sympify(txt) {
       fu = fu.replace(/\)x/gm,')*x');
       fu = fu.replace(/xx/gm,'x*x');
       fu = fu.replace(/xx/gm,'x*x');
+      fu = fu.replace(/xx/gm,'x*x');
+      fu = fu.replace(/xx/gm,'x*x');
+      fu = fu.replace(/\+\-/gm,'-');
+      fu = fu.replace(/\-\-/gm,'+');
+      fu = fu.replace(/\-\+/gm,'-');
+      fu = fu.replace(/^1\*/gm,'');
+      fu = fu.replace(/(^[0-9])1\*/gm,function(m,f) { return f; });
       fu = fu.replace(/\)\(/gm,')*(');
   return fu;
 }
