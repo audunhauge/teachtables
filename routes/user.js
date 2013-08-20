b82de48bb2508ce5882ed1df2bfad1f14c04e5e3
Audun Hauge <audun.hauge@gmail.com>
2013-08-20 09:04:43 +0200

    fixed gaff

--- routes/user.js
+++ routes/user.js
@@ -28,28 +28,6 @@ exports.saveconfig = function(req, res) {
   }
 };
 
-var aa=top.mainframe.document.getElementsByClassName("user_name");
-var b = aa.pop();
-var uname = b.innerHTML;
-uname = uname.split(",");
-uname = uname[1] + "%20" + uname[0];
-uname = uname.replace(/ /g,"%20");
-uname = uname.replace(/å/g,"%C3%A5");
-uname = uname.replace(/æ/g,"%C3%A6");
-uname = uname.replace(/ø/g,"%C3%B8");
-uname = uname.replace(/Å/g,"%C3%85");
-uname = uname.replace(/é/g,"%C3%A9");
-uname = uname.replace(/Ø/g,"%C3%98");
-uname = uname.replace(/Æ/g,"%C3%86");
-uname = uname.replace(/ü/g,"%C3%BC");
-var plain="http://www.teachtables.net/skeisvang/plain?navn="+uname;
-var starb="http://www.teachtables.net/skeisvang/starb?navn="+uname;
-var cal="http://www.teachtables.net/skeisvang/kalendar?navn="+uname;
-document.getElementById("timeplan").src=tar;
-function ssta() { document.getElementById("timeplan").src=plain;
-}function sstb() { document.getElementById("timeplan").src=starb;
-}function sstc() { document.getElementById("timeplan").src=cal;
-}
 exports.feide =function(req, res) {
   if (req.query.token) {
       var tok = req.query.token;
